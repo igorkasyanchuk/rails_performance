49d434eacd1ca67555597e149089f5d821bdf1f9
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2020-02-06 14:44:09 -0800

    fix

--- lib/rails_performance/rails/middleware.rb
+++ lib/rails_performance/rails/middleware.rb
@@ -11,15 +11,15 @@ module RailsPerformance
         if record = Thread.current["RP_request_info"]
           record[:status] ||= @status
 
-          rand(500).times do |e|
-            finished               = Time.now - rand(2000).minutes
-            record[:datetime]      = finished.strftime(RailsPerformance::FORMAT)
-            record[:datetimei]     = finished.to_i
-            record[:duration]      = 50 + rand(100) + rand(50.0) + rand(e)
-            record[:db_runtime]    = rand(50.0)
-            record[:view_runtime]  = rand(50.0)
-            RP::Utils.log_in_redis(record)
-          end
+          # rand(500).times do |e|
+          #   finished               = Time.now - rand(2000).minutes
+          #   record[:datetime]      = finished.strftime(RailsPerformance::FORMAT)
+          #   record[:datetimei]     = finished.to_i
+          #   record[:duration]      = 50 + rand(100) + rand(50.0) + rand(e)
+          #   record[:db_runtime]    = rand(50.0)
+          #   record[:view_runtime]  = rand(50.0)
+          #   RP::Utils.log_in_redis(record)
+          # end
 
           RP::Utils.log_in_redis(record)
 
