c54b41198c1c9ba951c5d9157f36ce68e2a57850
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2021-04-23 10:54:36 -0700

    use fallback if send_file disabled

--- lib/rails_performance/engine.rb
+++ lib/rails_performance/engine.rb
@@ -13,7 +13,11 @@ module RailsPerformance
       if ::Rails::VERSION::MAJOR.to_i >= 5
         app.middleware.insert_after ActionDispatch::Executor, RailsPerformance::Rails::Middleware
       else
-        app.middleware.insert_after ActionDispatch::Static, RailsPerformance::Rails::Middleware
+        begin
+          app.middleware.insert_after ActionDispatch::Static, RailsPerformance::Rails::Middleware
+        rescue
+          app.middleware.insert_after Rack::SendFile, RailsPerformance::Rails::Middleware
+        end
       end
       # look like it works in reverse order?
       app.middleware.insert_before RailsPerformance::Rails::Middleware, RailsPerformance::Rails::MiddlewareTraceStorerAndCleanup
