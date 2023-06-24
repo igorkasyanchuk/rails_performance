df9bf83c45cd50b3ef71bd7e2510cb66d827b023
Igor Kasyanchuk <igorkasyanchuk@gmail.com>
2023-06-24 21:23:28 +0300

    alpha3

--- lib/rails_performance/reports/response_time_report.rb
+++ lib/rails_performance/reports/response_time_report.rb
@@ -7,10 +7,10 @@ module RailsPerformance
 
       def data
         all     = {}
-        stop    = Time.at(60 * (Time.now.to_i / 60))
+        stop    = RailsPerformance::Reports::BaseReport::time_in_app_time_zone(Time.at(60 * (Time.now.to_i / 60)))
+        offset  = RailsPerformance::Reports::BaseReport::time_in_app_time_zone(Time.now).utc_offset
         current = stop - RailsPerformance.duration
         @data   = []
-        offset  = Time.now.utc_offset
 
         # puts "current: #{current}"
         # puts "stop: #{stop}"
@@ -25,8 +25,8 @@ module RailsPerformance
         # add blank columns
         while current <= stop
           views = all[current.strftime(RailsPerformance::FORMAT)] || 0
-          time = RailsPerformance::Reports::ResponseTimeReport::time_in_app_time_zone(current)
-          @data << [(time.to_i + offset) * 1000, views.round(2)]
+          # time = RailsPerformance::Reports::BaseReport::time_in_app_time_zone(current)
+          @data << [(current.to_i + offset) * 1000, views.round(2)]
           current += 1.minute
         end
 
--- lib/rails_performance/reports/throughput_report.rb
+++ lib/rails_performance/reports/throughput_report.rb
@@ -6,12 +6,15 @@ module RailsPerformance
         @group ||= :datetime
       end
 
+      # RailsPerformance::Reports::ThroughputReport.new(db).data
+      # Time.at(RailsPerformance::Reports::ThroughputReport.new(db).data.last[0] / 1000)
+
       def data
         all     = {}
-        stop    = Time.at(60 * (Time.now.to_i / 60))
+        stop    = RailsPerformance::Reports::BaseReport::time_in_app_time_zone(Time.at(60 * (Time.now.to_i / 60)))
+        offset  = RailsPerformance::Reports::BaseReport::time_in_app_time_zone(Time.now).utc_offset
         current = stop - RailsPerformance.duration
         @data   = []
-        offset  = Time.now.utc_offset
 
         # puts "current: #{current}"
         # puts "stop: #{stop}"
@@ -24,8 +27,7 @@ module RailsPerformance
         # add blank columns
         while current <= stop
           views = all[current.strftime(RailsPerformance::FORMAT)] || 0
-          time = RailsPerformance::Reports::ResponseTimeReport::time_in_app_time_zone(current)
-          @data << [(time.to_i + offset) * 1000, views.round(2)]
+          @data << [(current.to_i + offset) * 1000, views.round(2)]
           current += 1.minute
         end
 
