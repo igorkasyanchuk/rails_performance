7c784a70994d71cb0c07e85977bb282fb1afb679
Micah Geisel <micah@botandrose.com>
2025-04-07 11:32:47 +0100

    extract extract_signal method.

--- lib/rails_performance/reports/resources_report.rb
+++ lib/rails_performance/reports/resources_report.rb
@@ -31,6 +31,28 @@ module RailsPerformance
         end
       end
 
+      def cpu
+        @cpu ||= extract_signal { |e| e[:cpu]["one_min"].to_f.round(2) }
+      end
+
+      def memory
+        @memory ||= extract_signal { |e| e[:memory].to_f.round(2) }
+      end
+
+      def disk
+        @disk ||= extract_signal { |e| e[:disk]["available"].to_f.round(2) }
+      end
+
+      private
+
+      def extract_signal &block
+        data.transform_values do |v|
+          prepare_report(v.each_with_object({}) do |e, res|
+            res[e[:datetimei] * 1000] = block.call(e)
+          end)
+        end
+      end
+
       def data
         @data ||= db.data
           .collect { |e| e.record_hash }
@@ -38,32 +60,6 @@ module RailsPerformance
           # .transform_values { |v| v.sort { |a, b| b[sort] <=> a[sort] } }
           .transform_values { |v| v.map { |e| e.merge({datetimei: e[:datetimei].to_i}) } }
       end
-
-      def cpu
-        @cpu ||= data.transform_values do |v|
-          prepare_report(v.each_with_object({}) do |e, res|
-            res[e[:datetimei] * 1000] = e[:cpu]["one_min"].to_f.round(2)
-          end)
-        end
-      end
-
-      def memory
-        @memory ||= data.transform_values do |v|
-          prepare_report(v.each_with_object({}) do |e, res|
-            res[e[:datetimei] * 1000] = e[:memory].to_f.round(2)
-          end)
-        end
-      end
-
-      def disk
-        @disk ||= data.transform_values do |v|
-          prepare_report(v.each_with_object({}) do |e, res|
-            res[e[:datetimei] * 1000] = e[:disk]["available"].to_f.round(2)
-          end)
-        end
-      end
-
-      private
 
       def prepare_report(input)
         nullify_data(input, RailsPerformance.system_monitor_duration)
