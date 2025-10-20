851a1eafb76a954de1ca0b2d4d8540c8cc7f113c
Micah Geisel <micah@botandrose.com>
2025-10-20 15:09:13 -0600

    convert autoupdate to a custom element.

--- app/engine_assets/javascripts/autoupdate.js
+++ app/engine_assets/javascripts/autoupdate.js
@@ -1,35 +1,45 @@
-const recent = document.getElementById("recent");
-const autoupdate = document.getElementById("autoupdate");
-
-if (autoupdate) {
-  if (localStorage.getItem("autoupdate") === null) {
-    localStorage.setItem("autoupdate", "true");
+class RailsPerformanceAutoupdate extends HTMLElement {
+  connectedCallback() {
+    this.checkbox = this.querySelector('input[type="checkbox"]');
+    this.recentTable = document.getElementById('recent');
+    this.initializeCheckboxState();
+    this.setupCheckboxListener();
+    this.startPolling();
   }
-  autoupdate.checked = localStorage.getItem("autoupdate") === "true";
-  autoupdate.addEventListener('change', () => {
-    localStorage.setItem("autoupdate", autoupdate.checked);
-  });
-}
 
-if (recent) {
-  const tbody = recent.querySelector("tbody");
-
-  setInterval(() => {
-    const tr = tbody.children[0];
-    const from_timei = tr.getAttribute("from_timei") || '';
-
-    if (!autoupdate.checked) {
-      return;
+  initializeCheckboxState() {
+    if (localStorage.getItem('autoupdate') === null) {
+      localStorage.setItem('autoupdate', 'true');
     }
+    this.checkbox.checked = localStorage.getItem('autoupdate') === 'true';
+  }
 
-    fetch(`recent.js?from_timei=${from_timei}`, {
-      headers: {
-        "X-CSRF-Token": document.querySelector("[name='csrf-token']").content,
-      },
-    })
-      .then(res => res.text())
-      .then(html => {
-        tbody.innerHTML = html + tbody.innerHTML;
-      });
-  }, 3000);
+  setupCheckboxListener() {
+    this.checkbox.addEventListener('change', () => {
+      localStorage.setItem('autoupdate', this.checkbox.checked);
+    });
+  }
+
+  startPolling() {
+    const tbody = this.recentTable.querySelector('tbody');
+
+    setInterval(() => {
+      if(!this.checkbox.checked) return;
+
+      const tr = tbody.children[0];
+      const from_timei = tr.getAttribute('from_timei') || '';
+
+      fetch(`recent.js?from_timei=${from_timei}`, {
+        headers: {
+          'X-CSRF-Token': document.querySelector("[name='csrf-token']").content,
+        },
+      })
+        .then(res => res.text())
+        .then(html => {
+          tbody.innerHTML = html + tbody.innerHTML;
+        });
+    }, 3000);
+  }
 }
+
+customElements.define('rails-performance-autoupdate', RailsPerformanceAutoupdate);
